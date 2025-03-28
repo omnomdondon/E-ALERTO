c8de79622acabfde8035f9ecf84c6eda85e87ae9
Brandon Kenneth Cinco <brandon1203kennethdc@gmail.com>
2025-03-28 12:34:15 +0800

    UPDATED FEATURE IN BRANCH-6

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:e_alerto/controller/router.dart';
-import 'package:e_alerto/view/layout_scaffold.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-import '../view/screens/login_screen.dart';
-import 'view/screens/registration_screen.dart';
 
 void main() {
   runApp(const MainApp());
