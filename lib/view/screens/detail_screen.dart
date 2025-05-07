98ba4b269339d7097851e182727f8e2f2a768a9f
Brandon Kenneth Cinco <brandon1203kennethdc@gmail.com>
2025-05-08 00:02:19 +0800

    Auto-update: Merged upstream changes and local modifications

--- lib/view/screens/detail_screen.dart
+++ lib/view/screens/detail_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
